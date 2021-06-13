c1f997f23360c6382b65f22623d0b09830badb77
ben s <bsadick@gmail.com>
2021-06-13 23:12:50 +0900

    added customized auth for ui-react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import {AmplifyAuthenticator, AmplifySignUp, AmplifySignOut} from '@aws-amplify/ui-react'
+
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
+    <AmplifyAuthenticator>
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Amplify</h1>
+        <AmplifySignOut/>
       </header>
     </div>
+    <AmplifySignUp
+        headerText="My Custom Sign Up Text"
+        slot="sign-up"
+        formFields={[
+            { type: "username" },
+            {
+              type: "password",
+              label: "Password *",
+              placeholder: "Enter your password"
+            },
+            { type: "email" }
+          ]} 
+      ></AmplifySignUp>
+    </AmplifyAuthenticator>
   );
 }
 

8803f0983fee7b308a4408e0a4bf5f1f9e7c9337
ben s <bsadick@gmail.com>
2021-06-16 16:50:31 +0900

    more stuff added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
-import Amplify from 'aws-amplify';
+import Amplify, {API, graphqlOperation} from 'aws-amplify';
 import awsConfig from './aws-exports';
 import {AmplifyAuthenticator, AmplifySignUp, AmplifySignOut} from '@aws-amplify/ui-react'
-
-
+import {listLists} from './graphql/queries'
+import { useEffect, useState } from 'react';
+import 'semantic-ui/dist/semantic.min.css';
 Amplify.configure(awsConfig);
 
 function App() {
+  const[list, setList] = useState([])
+  async function fetchList(){
+    const {data} = await API.graphql(graphqlOperation(listLists));
+    setList(data.listLists.items)
+    console.log(data);
+  }
+  //fetch when application loads first time
+  //blank array for the above, specify variable to fetch when that var changes
+  useEffect(()=> {
+    fetchList()
+  }, [])
   return (
     <AmplifyAuthenticator>
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Amplify</h1>
+        <ul>
+          {list.map(item => <li key={item.id}>{item.title}</li>)}
+        </ul>
         <AmplifySignOut/>
       </header>
     </div>
